f4f0170d76204497b2cdc8261a661fe0c66ce05f
dev3 <tiago.inovatech.inf@gmail.com>
2021-07-21 11:28:18 -0300

    Displaying posts on home page

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\User::factory(10)->create(); // works
+
+
+        \App\Models\Post::factory(1)->create(); // works
+
+
+        // factory('App\User', 10)->create(); // this fucking shit does not work because Laravel sucks
     }
 }
