71ef6fdc9a29673de105cc07b6143bd16fb8976d
dev3 <tiago.inovatech.inf@gmail.com>
2021-07-30 10:04:11 -0300

    Displaying a post from admin - Part 2

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -495,9 +495,15 @@
 		</div>
 
 	@endsection
+
+
+	@section('scripts')
+		<!-- Page level plugins -->
+	  <script src="{{asset('vendor/datatables/jquery.dataTables.min.js')}}"></script>
+	  <script src="{{asset('vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+
+	  <!-- Page level custom scripts -->
+	  <script src="{{asset('js/datatables-demo.js')}}"></script>
+	@endsection
+
 </x-admin-master>
-
-
-
-
-
